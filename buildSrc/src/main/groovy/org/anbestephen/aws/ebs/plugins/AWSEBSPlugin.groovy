7d750b202bac3006c1b6d7265614c0dfeb50f763
Ambasager Estifanos <anbestephen@gmail.com>
2015-10-13 17:23:03 +0000

    Updated AWSEBSPlugin.groovy file

---
+++ buildSrc/src/main/groovy/org/anbestephen/aws/ebs/plugins/AWSEBSPlugin.groovy
@@ -0,0 +1,12 @@
+package org.anbestephen.aws.ebs.plugin
+
+import org.gradle.api.*;
+import org.gradle.api.tasks.*;
+
+class AWSEBSPlugin extends DefaultTask{
+
+  AWSEBSPlugin(){
+      
+  }    
+  
+}
